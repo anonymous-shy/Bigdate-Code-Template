db5a6865d1c1a69b15f6715704a2a085c4396b38
shy@mac <shenhuayu0417@gmail.com>
2023-03-06 10:49:10 +0800

    Initial commit

---
+++ CommonCode/src/main/java/xyz/shy/Main.java
@@ -0,0 +1,7 @@
+package xyz.shy;/**
+ * Created by ${USER} on ${DATE} ${DAY_NAME_FULL} ${TIME}
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
